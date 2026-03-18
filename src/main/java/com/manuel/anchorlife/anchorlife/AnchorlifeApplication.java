afbbc802a5a23734d3bb22be4cddbd5274c24e87
Manuel Aguirre <programadorjrsemad@gmail.com>
2026-03-17 20:27:51 -0600

    first commit

---
+++ src/main/java/com/manuel/anchorlife/anchorlife/AnchorlifeApplication.java
@@ -0,0 +1,13 @@
+package com.manuel.anchorlife.anchorlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnchorlifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manuel/anchorlife/anchorlife/AnchorlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.anchorlife.anchorlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
